cfcaa3801d7963a76a0806f9150feb7cc0dd0f49
Agung Kurniawan <agungklewang26@gmail.com>
2024-02-08 21:32:17 +0700

    test responsive

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
 
+void main() => runApp(const Main());
+
 class Main extends StatelessWidget {
   const Main({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    var screen = MediaQuery.of(context).size.width;
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: SafeArea(
             child: Center(
-          child: Text("HELLO KAMP SEWA"),
+          child: Text(
+            "$screen",
+            style: TextStyle(
+                fontSize: 20,
+                color: (screen < 380) ? Colors.black : Colors.blue),
+          ),
         )),
       ),
     );
